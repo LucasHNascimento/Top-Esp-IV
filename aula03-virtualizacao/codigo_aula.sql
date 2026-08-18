022bded3d5418cb615c80e97442a190c46796fe0
LucasHNascimento <130712881+LucasHNascimento@users.noreply.github.com>
2026-08-18 23:08:59 +0000

    códigos aula 18/08

---
+++ aula03-virtualizacao/codigo_aula.sql
@@ -0,0 +1,7 @@
+-- Active: 1787092925617@@127.0.0.1@5432@projeto@public
+CREATE TABLE pessoas (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telefone VARCHAR(20)
+);
